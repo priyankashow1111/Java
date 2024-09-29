be78858d64849705c4526888811ca5cd35bcfae0
priyankashow1111 <116818559+priyankashow1111@users.noreply.github.com>
2024-09-29 09:50:42 +0000

    Done

---
+++ Arrays/BubbleSort.java
@@ -0,0 +1,17 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a={36,19,29,12,5};
+        for(int i=0;i<a.length;i++) {
+            for(int j=0;j<a.length-1-i;j++) {
+                if(a[j]>a[j+1]) {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++) {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
